43625a49067a63ca88680ccb061d77c8a6592440
Benjamin Suk <benjamin.suk13@gmail.com>
2018-10-28 12:18:32 +0100

    Inital commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbotben-a8c1-Web-Deploy.pubxml -p:Password=4FDqRx66s5f6iXsvaSsdE8vvdMH8Tz932FpfKw3rgrpxZMoxco2q6gsRta5K
+
